7e87f17082b3eac9eaa86e46d923b67aa13ee43f
jasper <jaspersone@gmail.com>
2012-07-02 07:26:27 -0500

    Added basic structure for bar fill chart along with supporting css, not complete. Added functions file skeleton

---
+++ js/functions.js
@@ -0,0 +1,26 @@
+/*
+Title:   Basic Functions and Utilities Library for Lotus Charts
+Author:  Jasper Sone
+License: Unlimited use, reuse, modifications. Please just credit original authors.
+Updated: June 29, 2012
+Version: 0.0
+*/
+
+/****************************/
+/* HTML Parsing             */
+/****************************/
+
+/*  
+    Params: data_tuple_string - a string formated as a tuple that describes the chart
+    Return: a tuple of the chart data
+    Use:    Pass the string found in the vars section of each Lotus Charts div and
+            parse_chart_data_tuple will return a data structure that will describe and
+            control the chart values. This data structure can then be passed to
+            instanciate a Chart instance.
+*/
+
+function parse_chart_data_tuple (data_tuple_string) {
+    
+
+    return null;
+}
