619d8ead148a784a4bb1c9487d5def99cd16fd3d
jasper <jaspersone@gmail.com>
2012-07-10 08:42:39 -0500

    Added skeleton file for functions js as well as less css js file

--- js/functions.js
+++ js/functions.js
@@ -1,26 +1,117 @@
-/*
-Title:   Basic Functions and Utilities Library for Lotus Charts
-Author:  Jasper Sone
-License: Unlimited use, reuse, modifications. Please just credit original authors.
-Updated: June 29, 2012
-Version: 0.0
-*/
+/************************************
+* Lotus Charts Functions JS v0.01   *
+* July 10, 2012                     *
+************************************/
 
-/****************************/
-/* HTML Parsing             */
-/****************************/
+/************************************
+* Global Variables  	   	 	    *
+************************************/
+var TESTING = true;
 
-/*  
-    Params: data_tuple_string - a string formated as a tuple that describes the chart
-    Return: a tuple of the chart data
-    Use:    Pass the string found in the vars section of each Lotus Charts div and
-            parse_chart_data_tuple will return a data structure that will describe and
-            control the chart values. This data structure can then be passed to
-            instanciate a Chart instance.
-*/
-
-function parse_chart_data_tuple (data_tuple_string) {
-    
-
-    return null;
+/************************************
+* Screen Information  	   	 	    *
+************************************/
+// params: none
+// return: true if the device has a pixel density greater than or equal to 2, false otherwise
+function isRetinaScreen() {
+	return window.devicePixelRatio >= 2;
 }
+
+//	params: none
+//	return: array of integer screen dimensions given as [width, height]
+function getScreenDimensions() {
+	var dimensions = [$('body').width(), $('body').height()];
+	return dimensions;
+}
+
+/************************************
+* Cookies                           *
+************************************/
+
+/*
+Notes:
+In order for this cookie library to work properly, all cookies
+on the site should be set using the setCookie function. This
+insures that proper formatting of cookies is set in the document.
+If other cookies are being set (or created) without the use of
+setCookie, it is possible that they may not appear in the results
+from getCookie (because of how a cookies key and value are stored).
+*/
+
+// params: name = cookie key, value = cookie value, days = amount of days cookie should last 
+// return: none
+// behavior: given the name, value, and days, a cookie is set with the expiration of the current
+//			 time plus the amount of days passed in as a param.
+function setCookie(name, value, days) {
+    var expires = '';
+    if (days) {
+        var expirationDate = new Date();
+        expirationDate.setTime(expirationDate.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = '; expires=' + expirationDate.toGMTString();
+    }
+    document.cookie = name + '=' + value + expires + '; path=/';
+}
+
+// params: name = cookie key
+// return: the value of the cookie if it exists, null otherwise
+function getCookie(name) {
+    var key = name + '=';
+    var cookieDict = document.cookie.split(';');
+    for (var i = 0; i < cookieDict.length; i++) { 
+        var current = cookieDict[i];
+        while (current.charAt(0)==' ') { // TODO: refactor this loop, replace with regex
+            current = current.substring(1,current.length);
+        }
+        if (current.indexOf(key) == 0) {
+            return current.substring(key.length, current.length);
+        }
+    }
+    return null; // return null if current key is not found
+}
+
+// params: name = cookie key
+// return: none
+// behavior: deletes cookie with key == name by setting its expiration as yesterday
+function delCookie(name) {
+    setCookie(name, '', -1);
+}
+
+/************************************
+* Image Preloading                  *
+************************************/
+
+// Array of images:
+var imageArray = ['/media/search-spinner.gif', '/media/hr-search.png'];
+ 
+// Loop through and declare an element that will not actually be used.
+$.each(imageArray, function (i, val) {
+  $('<img/>').attr('src', val);
+});
+
+/************************************
+* Main                              *
+************************************/
+
+// Main function calls
+$(document).ready(function() {
+
+	// get screen information
+    var is_retina = isRetinaScreen();
+    var screen_dimensions = getScreenDimensions(); // array [width, height]
+	
+	if (TESTING) {
+		console.log("Initial Screen Dimensions are: " + screen_dimensions);
+	}
+	
+	// keep screen_dimensions up to date
+	window.onresize = function() {
+		screen_dimensions = getScreenDimensions();
+		if (TESTING)
+			console.log("New screen dimensions are: " + screen_dimensions);
+	}
+	
+	// check for internet explorer
+    if ($.browser.msie) {
+		alert("Hello IE, we meet again.")
+	}
+});
